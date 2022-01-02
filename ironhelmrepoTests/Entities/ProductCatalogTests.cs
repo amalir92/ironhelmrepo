ede84357a48d61f6a75cd7c8eb715f9018289c89
amalir92 <amalirathnapriya@gmail.com>
2022-01-02 22:33:51 +0000

    tests

---
+++ ironhelmrepoTests/Entities/ProductCatalogTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Iron_helm_order_mgt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iron_helm_order_mgt.Tests
+{
+    [TestClass()]
+    public class ProductCatalogTests
+    {
+        [TestMethod()]
+        public void ProductCatalogTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ProductCatalogTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ProductCatalogTest2()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void createNewProductTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getAllProductsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getProductByIdTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
--- ironhelmrepoTests/Properties/AssemblyInfo.cs
+++ ironhelmrepoTests/Properties/AssemblyInfo.cs
@@ -2,38 +2,35 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ironhelmrepo")]
+[assembly: AssemblyTitle("ironhelmrepoTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ironhelmrepo")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyProduct("ironhelmrepoTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("975694ca-a913-4594-be74-58562964ab1d")]
+[assembly: Guid("22b59412-50e1-4efa-bd03-420e329440ca")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("ironhelmrepo.Tests")]
-[assembly: InternalsVisibleTo("ironhelmrepo.Explorables")]
-[assembly: InternalsVisibleTo("ironhelmrepo.Tests1")]
