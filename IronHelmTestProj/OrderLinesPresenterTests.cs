73048f4db3acd9f451bd4304d051c4642c052653
unknown <mail2udani@gmail.com>
2022-01-02 18:27:37 +0000

    added presentation layer test cases

--- IronHelmTestProj/OrderLinesPresenterTests.cs
+++ IronHelmTestProj/OrderLinesPresenterTests.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Iron_helm_order_mgt;
+using ironhelmrepo.Presenters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Data;
 
 namespace IronHelmTestProj
 {
@@ -9,6 +12,16 @@ namespace IronHelmTestProj
         [TestMethod]
         public void TestMethod1()
         {
-        }
+            OrderLIneViewscreenMoc moc = new OrderLIneViewscreenMoc();
+            OrderLineItem item = new OrderLineItem();
+            Order or = new Order();
+            or.orderId = 7;
+            item.OrderId = or;
+            OrderLinesPresenter presenter = new OrderLinesPresenter(moc,item);
+            moc.clientId = "001C";
+            moc.orderId = 7;
+            DataTable dt = presenter.getOrderLines();
+            Assert.IsTrue(dt.Rows.Count>0);
+    }
     }
 }
